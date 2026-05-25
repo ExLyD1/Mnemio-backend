eab738f8a9592aa2b15e57f7964d5e3b2b6153dd
ExLyD1_qq <sashaahnij@gmail.com>
2026-05-25 20:04:05 +0300

    chore(foundation): remove stale user stubs and pg pool

--- src/routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyInstance } from 'fastify';
-import { createUser } from '../controllers/user.controller.js';
-
-const userRoutes = async (fastify: FastifyInstance) => {
-    fastify.post('/create-user', createUser);
-};
-
-export default userRoutes;
--- src/services/user.service.ts
+++
@@ -1,6 +0,0 @@
-import * as userRepository from '../repositories/user.repository.js';
-export const createUser = async (userData: any) => {
-    const user = await userRepository.createUser(userData);
-
-    return user;
-};
